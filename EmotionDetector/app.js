0fbb6535cc24c077b749cb81bbcc047cde56e8ab
Mahdi Zouch <zouma45@hotmail.com>
2016-05-09 19:51:53 +0000

    Changes made by Mahdi Zouch (zouma45@hotmail.com).

--- EmotionDetector/app.js
+++ EmotionDetector/app.js
@@ -46,4 +46,8 @@
 // START_CUSTOM_CODE_kendoUiMobileApp
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
 
+
+  
+
+
 // END_CUSTOM_CODE_kendoUiMobileApp
